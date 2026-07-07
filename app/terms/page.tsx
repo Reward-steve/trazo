c90094a8c733933e4be075df396fbc40021495a4
Reward Steve <rewardstephen30@gmail.com>
2026-07-07 16:56:14 +0100

    fix

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import Image from "next/image";
+import logo from "../../public/trazo_omega.png"
 
 export const metadata: Metadata = {
   title: "Terms of Service — Trazo",
